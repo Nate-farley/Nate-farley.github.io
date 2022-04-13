0dcf0ed788b0f9475a4963c38ea155213da608b4
Nathan Farley <nathanfarley@Nathans-MacBook-Pro.local>
2022-04-13 02:08:05 -0500

    Redeployment of portfolio files.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import BubbleBackground from './components/BubbleBackground/BubbleBackground';
+import LandingPage from './components/LandingPage/LandingPage';
+import Portfolio from './components/Portfolio/Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+            <BubbleBackground />
+      <NavBar />
+      
+      <LandingPage />
+
+      <Portfolio />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        title: 'Pacman in React',
+        description: 'An implementation of Pacman built with React.',
+        tools: [1,2,3],
+        link: 'https://github.com/Nate-farley/Pac-Man'
+    },
+    {
+        title: 'Eyes in Motion',
+        description: 'Mouse tracking using browser and native DOM elements.',
+        tools: [1,2,3],
+        link: 'https://github.com/Nate-farley/eyes-in-motion'
+    },
+    {
+        title: 'Enhanced Bus Tracking',
+        description: 'Full visual of the MIT bus system in real time.',
+        tools: [1,2,3],
+        link: 'https://github.com/Nate-farley/bus-tracker-animation'
+    },
+]
+
+export default projects
