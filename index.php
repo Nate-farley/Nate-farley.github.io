c288feb4d5b8aea6e46181ea53bb3765af238d74
Nathan Farley <nathanfarley@Nathans-MBP.lan>
2022-08-12 22:48:08 -0400

    update personal pages

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php 
 
-    include_once(“home.html”);
-    include_once(“page-portfolio-amazon-clone-detail.html”);
-    include_once(“page-portfolio-badbanking-detail.html”);
-    include_once(“home.html”);
-    include_once(“home.html”);
-    include_once(“home.html”);
-    include_once(“home.html”);
-    include_once(“home.html”);
+    include_once(“index.html”);
+   
 
 ?>
